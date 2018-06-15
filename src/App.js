99812a016ca3be9b694b941cfd5a84e738a8d8db
Aimable <niyoai@yahoo.fr>
2018-06-15 13:41:16 +0100

    Stateless Components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SkiDayCount from './components/SkiDayCount';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +7,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SkiDayCount
+            total={50}
+            powder={20}
+            backcountry={10}
+            goal={100}
+        />
       </div>
     );
   }
---
+++ src/components/SkiDayCount.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../stylesheets/ui.scss';
+
+//Stateless functional components
+
+const percentToDecimal = (decimal) =>{
+    return ((decimal * 100) + '%');
+};
+
+const calcGoalProgress = (total, goal) => {
+    return percentToDecimal(total/goal);
+};
+const SkiDayCount = ({total, powder, backcountry, goal}) => (
+        <div className="ski-day-count">
+            <div className="total-days">
+                <span>{total}</span>
+                <span>days</span>
+            </div>
+            <div className="powder-days">
+                <span>{powder}</span>
+                <span>days</span>
+            </div>
+            <div className="backcountry-days">
+                <span>{backcountry}</span>
+                <span>hiking day</span>
+            </div>
+            <div>
+                    <span>{calcGoalProgress(
+                        total,
+                        goal)
+                    }
+                           </span>
+            </div>
+        </div>
+    );
+
+export default SkiDayCount;
